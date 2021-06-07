7e74ade3f3d07ab606dac1ad70629c8b0825ef8f
Anderson Alves Nunes de Freitas <anunes@instructure.com>
2021-06-07 04:01:33 -0300

    mini menu

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,7 +1,14 @@
+// noinspection HtmlUnknownTarget
+
 import React, { FC } from 'react'
 
 const Nav: FC = () => {
-  return <div>I'm the nav!</div>
+  return (
+    <ul style={{ listStyle: 'none', display: 'flex' }}>
+      <li><a href="/">Home</a></li>&nbsp;&nbsp;
+      <li><a href="/login">Login</a></li>
+    </ul>
+  )
 }
 
 export default Nav
